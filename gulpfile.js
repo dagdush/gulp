5350d09a49f40df40582fcad3fb9f148edf1cc59
dagdush <dagdush@gmail.com>
2016-03-25 18:00:25 +0200

    config gulp and package.json files

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,8 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var notify = require("gulp-notify");
 
-/*SASS*/
-gulp.task('my', function () {
+    //sass
+gulp.task('styles', function () {
     return gulp.src('./styles/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer('last 2 version'))
@@ -19,22 +19,24 @@ gulp.task('my', function () {
         .pipe(csso({restructure: false,sourceMap: true, debug: true}))
         .pipe(notify("Gulp css Done!"));
 });
-/*js*/
+    //js
 gulp.task('scripts', function() {
     return gulp.src('./scripts/*.js')
         .pipe(concat('all.js'))
         .pipe(gulp.dest('./scripts/'))
         .pipe(uglify())
         .pipe(rename({suffix: ".min"}))
-        .pipe(gulp.dest('./scripts/min'));
-});
-/*sass watch*/
-gulp.task('sass:watch', function () {
-    gulp.watch('./styles/**/*.scss', ['sass']);
+        .pipe(gulp.dest('./scripts/min'))
+        .pipe(notify("Gulp css Done!"));
 });
 
-/*default*/
-gulp.task('default', function() {
-    // place code for your default task here
+    //watch
+gulp.task('watch', function() {
+
+    // Watch .scss files
+    gulp.watch('./styles/**/*.scss', ['styles']);
+
+    // Watch .js files
+    gulp.watch('./scripts/**/*.js', ['scripts']);
 });
 
